0d50c07b8bae8098c5416c778b4c209885837fad
Harsh <harshvardhan@bytemark.co>
2019-04-08 01:32:15 +0530

    Added Card View

--- GridWithBackendAPI/js/index.js
+++ GridWithBackendAPI/js/index.js
@@ -1,5 +1,5 @@
 
-let products;
+let products, brandProducts;
 
 var a = document.getElementById('grid');
 
@@ -8,8 +8,110 @@ function getProducts(){
     .then(Response => Response.json())
     .then( data => {
         products = data.products;
-        makeDOMElement(products);
+        var brands = [];
+        // makeDOMElement(products);
+        makeDOMElement2(products);
+        for(let i=0; i<products.length; i++){
+            brands.push(products[i].brand);
+        }
+        brandProducts = brands.filter(onlyUnique);
+        brandList(brandProducts);
     }) 
+}
+
+function makeDOMElement2(data){
+
+    for(let i=0; i<data.length; i++){
+        var div1 = document.createElement('div');
+        div1.setAttribute("class","card")
+        var img1 = document.createElement('img');
+        img1.setAttribute("src", data[i].image);
+        img1.setAttribute("width", "100%");
+    
+        var header1 = document.createElement('div');
+        header1.setAttribute("class", "header");
+        header1.innerHTML = data[i].title;
+        var para1 = document.createElement('p');
+
+        para1.setAttribute("class","price");
+        para1.innerHTML = `&#8360 ${data[i].price.final_price}`;
+
+        header1.appendChild(para1);
+        var para3 = document.createElement('p');
+        var button1 = document.createElement('button');
+        button1.innerHTML = "Add To Cart"
+        para3.appendChild(button1);
+    
+        div1.appendChild(img1);
+        div1.appendChild(header1);
+        div1.appendChild(para3);
+    
+        a.appendChild(div1);
+    }
+}
+
+function brandList(data){
+    var numOfColumns;
+    if(data.length % 10 === 0){ numOfColumns = data.length / 10;}
+    else {numOfColumns = (data.length / 10) + 1;}
+    var divBrand = document.getElementById('brandList');
+    var table = document.createElement('table');
+    divBrand.appendChild(table);
+    for(let i=0; i < 10; i++){
+        var tr1 = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var td4 = document.createElement('td');
+        var td5 = document.createElement('td');
+        var td6 = document.createElement('td');
+
+
+        var text11 = document.createTextNode('Text1');
+        var text12 = document.createTextNode('Text2');
+        var text13 = document.createTextNode('Text3');
+        var text14 = document.createTextNode('Text4');
+        var text15 = document.createTextNode('Text5');
+        var text16 = document.createTextNode('Text6');
+
+        if(data[i]!== 'undefined'){text11.nodeValue = data[i]};
+        if(data[i+10] !== 'undefined'){text12.nodeValue = data[i+10]};
+        if(data[i+20] !== 'undefined'){text13.nodeValue = data[i+20]};
+        if(data[i+30] !== 'undefined'){text14.nodeValue = data[i+30]};
+        if(data[i+40] !== 'undefined'){text15.nodeValue = data[i+50]};
+        if(data[i+50] !== 'undefined'){text16.nodeValue = data[i+60]};
+        
+        var x = document.createElement("INPUT");
+        x.setAttribute("type", "checkbox");
+        x.setAttribute("value", "a");
+
+        td1.innerHTML+= x.outerHTML;
+        td2.innerHTML+= x.outerHTML;
+        td3.innerHTML+= x.outerHTML;
+        td4.innerHTML+= x.outerHTML;
+        td5.innerHTML+= x.outerHTML;
+        td6.innerHTML+= x.outerHTML;
+
+        td1.appendChild(text11);
+        td2.appendChild(text12);
+        td3.appendChild(text13);
+        td4.appendChild(text14);
+        td5.appendChild(text15);
+        td6.appendChild(text16);
+
+        
+        if(numOfColumns >= 1){ tr1.appendChild(td1);}
+        if(numOfColumns >= 2){ tr1.appendChild(td2);}
+        if(numOfColumns >=3){ tr1.appendChild(td3);}
+        if(numOfColumns >= 4){ tr1.appendChild(td4);}
+        if(numOfColumns >= 5){ tr1.appendChild(td5);}
+        if(numOfColumns >= 6){ tr1.appendChild(td6);}
+
+        table.appendChild(tr1);
+        
+    }
+    console.log(table);
+
 }
 
 function searchValue(){
@@ -18,7 +120,7 @@ function searchValue(){
     while (a.firstChild) {
         a.removeChild(a.firstChild);
     }
-    makeDOMElement(result);
+    makeDOMElement2(result);
 }
 
 function priceLowToHigh(data){
@@ -26,12 +128,18 @@ function priceLowToHigh(data){
         a.removeChild(a.firstChild);
     }
     if(data === 1){
-        makeDOMElement(products.sort(function(a, b){return a.price.final_price - b.price.final_price}));
+        makeDOMElement2(products.sort(function(a, b){return a.price.final_price - b.price.final_price}));
     }
     else if(data === 2){
-        makeDOMElement(products.sort(function(a, b){return b.price.final_price  - a.price.final_price }));
+        makeDOMElement2(products.sort(function(a, b){return b.price.final_price  - a.price.final_price }));
     }
 }
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+
 
 function makeDOMElement(data){
     if(data.length > 0) {
@@ -104,6 +212,13 @@ function appendRowsForColummn(data, table){
         table.appendChild(tr);
     }
 }
+
+function openModal() { 
+    document.getElementById("myDialog").showModal(); 
+  } 
+function closeModal(){
+       document.getElementById("myDialog").close();
+  }
 
 getProducts();
 
