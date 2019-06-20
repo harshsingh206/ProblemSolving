daeff236e296aa08d09eaac31d8184e2c4d59362
Harsh <harshvardhan@bytemark.co>
2019-06-20 13:43:21 +0530

    Added new file

--- HackerRank/Array/SellStockMaxProfit.js
+++ HackerRank/Array/SellStockMaxProfit.js
@@ -1,16 +1,22 @@
+
 var price = [100, 180, 260, 310, 40, 535, 695];
 var minimum=0, maximum=0,i=0,n, count=0;
 n = price.length;
 
-while (i < n - 1) { 
-    while ((i < n - 1) && (price[i + 1] <= price[i])) 
-    if (i == n - 1) 
-        break; 
-    minimum = i++; 
-    while ((i < n) && (price[i] >= price[i - 1])) 
-        i++; 
-    maximum = i - 1; 
-    count++; 
-} 
-
-console.log(`${minima1} and ${maxima1}`)
+if(n === 1){
+    console.log("Need atleast 2 day data");
+}
+else{
+    while (i < n - 1) { 
+        while ((i < n - 1) && (price[i + 1] <= price[i])) 
+        if (i == n - 1) 
+            break; 
+        minimum = i++; 
+        while ((i < n) && (price[i] >= price[i - 1])) 
+            i++; 
+        maximum = i - 1; 
+        count++; 
+    } 
+    
+    console.log(`${minimum} and ${maximum}`)
+}
